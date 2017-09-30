e74d0bf13d220487ca8170f19b8feb8189c6ce1e
raz <ma.raz1900@gmail.com>
2017-09-30 13:02:03 -0400

    Switch statement

---
+++ src/lessonOne/DataStructure.java
@@ -0,0 +1,16 @@
+package lessonOne;
+
+/**
+ * Created by raz on 9/20/17.
+ */
+public class DataStructure {
+    public static void main(String[] args) {
+        String one="java is the most populus language";
+        char[]charray=one.toCharArray();
+        for (int i=charray.length-1;i>=0;i--){
+            System.out.print(charray[i]);
+        }
+
+    }
+
+}
