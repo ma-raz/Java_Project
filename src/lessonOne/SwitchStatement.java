7d513e6b888a34e495047c5b51f485a79489f77d
raz <ma.raz1900@gmail.com>
2017-09-30 13:03:39 -0400

    Switch statement

---
+++ src/lessonOne/SwitchStatement.java
@@ -0,0 +1,39 @@
+package lessonOne;
+
+/**
+ * Created by raz on 9/20/17.
+ */
+public class SwitchStatement {
+    public static void main(String[] args) {
+        int x=30;
+
+           switch (x){
+               case 10:
+               {
+                   System.out.println("value of x is 10");
+                   break;
+               }
+               case 20:
+               {
+                   System.out.println("value of x is 20 ");
+                   break;
+               }
+               case 30:
+               {
+                   System.out.println("value of x is 30 ");
+                   break;
+               }
+              default:
+               {
+                   System.out.println("value not match");
+               }
+           }
+
+
+
+
+
+
+        }
+    }
+
